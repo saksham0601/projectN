6de9cd55f2f1a3e00b0a39215f88df1bd86a3df2
saksham0601 <saksham0601@gmail.com>
2023-11-09 22:23:54 -0800

    formatting using buttons

--- 02_demo/01_scripts/text.js
+++ 02_demo/01_scripts/text.js
@@ -10,3 +10,54 @@ document.addEventListener('DOMContentLoaded', () => {
       noteDisplay.innerHTML = noteHTML; // Display the rendered HTML
   });
 });
+
+function applyBold() {
+  var selectedText = window.getSelection().toString();
+  if (selectedText) {
+      var modifiedText = "**"+selectedText+"**";
+      replaceSelection(modifiedText);
+  }
+}
+
+function applyItalic() {
+  var selectedText = window.getSelection().toString();
+  if (selectedText) {
+      var modifiedText = "*"+selectedText+"*";
+      replaceSelection(modifiedText);
+  }
+}
+
+function applyUnderline() {
+  var selectedText = window.getSelection().toString();
+  if (selectedText) {
+      var modifiedText = "_"+selectedText+"_";
+      replaceSelection(modifiedText);
+  }
+}
+
+function getSelectedText() {
+  var text = "";
+  if (window.getSelection) {
+      text = window.getSelection().toString();
+  } else if (document.selection && document.selection.type !== "Control") {
+      text = document.selection.createRange().text;
+  }
+  return text;
+}
+
+// This function replaces the selected text
+function replaceSelection(selection) {
+  // Get the textarea element by its ID
+  var textBox = document.getElementById("noteInput");
+
+  // Get the start and end positions of the selected text
+  var startPos = textBox.selectionStart;
+  var endPos = textBox.selectionEnd;
+
+  // Get the text before and after the selected text
+  var textBefore = textBox.value.substring(0, startPos);
+  var textAfter = textBox.value.substring(endPos, textBox.value.length);
+
+  // Set the value of the textBox with the selected text removed
+  textBox.value = textBefore + selection + textAfter;
+}
