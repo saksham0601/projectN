983c74007b30fd4af149070715067be833fad850
yuli-cloud <136275512+yuli-cloud@users.noreply.github.com>
2023-12-04 00:04:04 -0800

    sfasdf

--- 02_demo/01_scripts/script.js
+++ 02_demo/01_scripts/script.js
@@ -67,13 +67,13 @@ function newDocument() {
 function changeTheme(theme) {
     const body = document.body;
     const nav = document.querySelector('nav');
-    const sidebar = document.querySelector('sidebar')
-
+    const sidebar = document.querySelector('sidebar');
     switch (theme) {
         case 'default':
             body.style.backgroundColor = '#31313f';
             body.style.color = 'white';
             nav.style.backgroundColor = '#31313f';
+            nav.style.boxShadow = '0 0 10px #1C1C23';
             const dropdown1 = document.querySelectorAll('.nav-listitemdrop');
             dropdown1.forEach(dropdown => {
                 dropdown.style.backgroundColor = '#383845';
@@ -83,10 +83,31 @@ function changeTheme(theme) {
             body.style.backgroundColor = '#f9f9f9';
             body.style.color = '#333';
             nav.style.backgroundColor = 'white';
+            nav.style.boxShadow = '0 0 10px lightgrey';
             const dropdowns = document.querySelectorAll('.nav-listitemdrop');
             dropdowns.forEach(dropdown => {
                 dropdown.style.backgroundColor = 'white';
             });
+
+            // Navigation button styles
+            const lightNavigationButtons = document.querySelectorAll('.navigation button');
+            lightNavigationButtons.forEach(button => {
+                button.style.backgroundColor = 'white';
+                button.style.boxShadow = '0 0 10px lightgray';
+                button.style.color = 'black';
+            });
+
+            const lightFlashcardInputs = document.querySelectorAll('.flashcardForm input[type="text"]');
+            lightFlashcardInputs.forEach(input => {
+                input.style.backgroundColor = 'white';
+                input.style.boxShadow = '0 0 10px lightgray';
+                input.style.color = 'black';
+            });
+            const lightFlashcardformbutton = document.querySelectorAll('.flashcardForm button');
+            lightFlashcardformbutton.forEach(button => {
+                button.style.color = 'black';
+            });
+            
             break;
         default:
             break;
@@ -136,20 +157,20 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
-   // Save selected theme to localStorage
-   function saveTheme(theme) {
+// Save selected theme to localStorage
+function saveTheme(theme) {
     localStorage.setItem('selectedTheme', theme);
-  }
+}
 
-  // Retrieve selected theme from localStorage
-  function getSavedTheme() {
+// Retrieve selected theme from localStorage
+function getSavedTheme() {
     return localStorage.getItem('selectedTheme');
-  }
+}
 
-  // Apply the saved theme on page load
-  document.addEventListener('DOMContentLoaded', function () {
+// Apply the saved theme on page load
+document.addEventListener('DOMContentLoaded', function () {
     const savedTheme = getSavedTheme();
     if (savedTheme) {
-      changeTheme(savedTheme);
+        changeTheme(savedTheme);
     }
-  }); 
+}); 
